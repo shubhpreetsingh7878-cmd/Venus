f46c38c3ab80169322704f54d0d136c3f6c313da
shubh <shubh@gmail.com>
2026-04-22 12:48:29 +0530

    update

--- src/About.jsx
+++ src/About.jsx
@@ -4,6 +4,10 @@ import Section from "./Section"
 const About = () => {
   return (
     <>
+      <div className="mt-20">
+         <h1 className="text-4xl text-[#081738] text-center font-bold">About Us</h1>
+         <p className="text-center text-xl mt-10 text-gray-500">Discover a wealth of insightful materials meticulously crafted to provide you <br /> with a comprehensive understanding of the latest trends.</p>
+      </div>
       <Section/>
       <Sectiontwo/> 
     </>
--- src/Servicepage.jsx
+++ src/Servicepage.jsx
@@ -4,6 +4,10 @@ import Services from "./Services"
 const Servicepage = () => {
   return (
     <>
+      <div className="mt-20">
+         <h1 className="text-4xl text-[#081738] text-center font-bold">Services</h1>
+         <p className="text-center text-xl mt-10 text-gray-500">Discover a wealth of insightful materials meticulously crafted to provide you <br /> with a comprehensive understanding of the latest trends.</p>
+      </div>
       <Services/> 
        
     </>
